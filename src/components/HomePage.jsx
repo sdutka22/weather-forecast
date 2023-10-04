b294348e6f195a8522a905a20bf8cb6071615be5
Szymon Dutka <119742288+sdutka22@users.noreply.github.com>
2023-10-04 15:10:04 +0200

    Added daily weather cards

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,14 +1,20 @@
 import React from 'react';
 import Navbar from './Navbar';
-import { Container, Typography, Paper, Grid } from '@mui/material';
+import { Container, Typography, Paper, Grid, Card, CardContent, Box } from '@mui/material';
+import WbSunnyIcon from '@mui/icons-material/WbSunny';
+import WavesIcon from '@mui/icons-material/Waves';
+import WbTwilightIcon from '@mui/icons-material/WbTwilight';
+import AirIcon from '@mui/icons-material/Air';
+import WaterDropIcon from '@mui/icons-material/WaterDrop';
+import ThermostatIcon from '@mui/icons-material/Thermostat';
 
 const HomePage = () => {
-  const cityName = 'Krakow'; // Zmień na odpowiednie dane
-  const countryName = 'Poland'; // Zmień na odpowiednie dane
+  const cityName = 'Krakow';
+  const countryName = 'Poland';
   const currentDate = new Date();
   const currentTemperature = 11;
   const minTemperature = 10;
-  const maxTemperature = 15; // Zmień na odpowiednie dane
+  const maxTemperature = 15;
 
   const formattedDate = new Intl.DateTimeFormat('en-US', {
     weekday: 'long',
@@ -21,7 +27,7 @@ const HomePage = () => {
   }).format(currentDate);
 
   return (
-    <Paper sx={{ minHeight: '100vh', backgroundColor: '#171717' }}>
+    <Paper sx={{ minHeight: '200vh', backgroundColor: '#171717' }}>
       <Navbar />
       <Container maxWidth="md" sx={{ marginTop: '50px', textAlign: 'center' }}>
         <Typography variant="h2" sx={{ color: '#fff', marginBottom: '10px', fontFamily: 'Poppins, sans-serif' }}>
@@ -47,8 +53,98 @@ const HomePage = () => {
               </Typography>
             </Grid>
         </Grid>
-        <Paper  sx={{ padding: '20px', backgroundColor: '#1e1e1e' }}>
-        </Paper>
+        <Grid container spacing={2} sx={{ marginTop: '20px'}}>
+            <Grid item xs={12} md={6}>
+              <Card sx={{ backgroundColor: '#1e1e1e', marginBottom: '10px', borderRadius: '10px' }}>
+                <CardContent>
+                  <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+                    <Typography variant="h6" gutterBottom sx={{ color: '#fff' }}>
+                      Sunrise
+                    </Typography>
+                    <WbSunnyIcon sx={{ fontSize: 48, color: '#fff', marginBottom: '10px' }} />
+                  </Box>
+                  <Typography variant="body1" sx={{ textAlign: 'center', color: '#fff', fontSize: '24px' }}>
+                    11:30 PM
+                  </Typography>
+                </CardContent>
+              </Card>
+            </Grid>
+            <Grid item xs={12} md={6}>
+              <Card sx={{ backgroundColor: '#1e1e1e', marginBottom: '10px', borderRadius: '10px' }}>
+                <CardContent>
+                  <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+                    <Typography variant="h6" gutterBottom sx={{ color: '#fff' }}>
+                      Sunset
+                    </Typography>
+                    <WbTwilightIcon sx={{ fontSize: 48, color: '#fff', marginBottom: '10px' }} />
+                  </Box>
+                  <Typography variant="body1" sx={{ textAlign: 'center', color: '#fff', fontSize: '24px' }}>
+                    6:30 PM
+                  </Typography>
+                </CardContent>
+              </Card>
+            </Grid>            
+            <Grid item xs={12} md={6}>
+              <Card sx={{ backgroundColor: '#1e1e1e', marginBottom: '10px', borderRadius: '10px' }}>
+                <CardContent>
+                  <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+                    <Typography variant="h6" gutterBottom sx={{ color: '#fff' }}>
+                        UV Index
+                    </Typography>
+                    <ThermostatIcon sx={{ fontSize: 48, color: '#fff', marginBottom: '10px' }} />
+                  </Box>
+                  <Typography variant="body1" sx={{ textAlign: 'center', color: '#fff', fontSize: '24px' }}>
+                    5
+                  </Typography>
+                </CardContent>
+              </Card>
+            </Grid>   
+            <Grid item xs={12} md={6}>
+              <Card sx={{ backgroundColor: '#1e1e1e', marginBottom: '10px', borderRadius: '10px' }}>
+                <CardContent>
+                  <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+                    <Typography variant="h6" gutterBottom sx={{ color: '#fff' }}>
+                        Rain Sum
+                    </Typography>
+                    <WaterDropIcon sx={{ fontSize: 48, color: '#fff', marginBottom: '10px' }} />
+                  </Box>
+                  <Typography variant="body1" sx={{ textAlign: 'center', color: '#fff', fontSize: '24px' }}>
+                    10 mm
+                  </Typography>
+                </CardContent>
+              </Card>
+            </Grid>            
+            <Grid item xs={12} md={6}>
+              <Card sx={{ backgroundColor: '#1e1e1e', marginBottom: '10px', borderRadius: '10px' }}>
+                <CardContent>
+                  <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+                    <Typography variant="h6" gutterBottom sx={{ color: '#fff' }}>
+                        Max Wind Speed
+                    </Typography>
+                    <AirIcon sx={{ fontSize: 48, color: '#fff', marginBottom: '10px' }} />
+                  </Box>
+                  <Typography variant="body1" sx={{ textAlign: 'center', color: '#fff', fontSize: '24px' }}>
+                    20 m/s
+                  </Typography>
+                </CardContent>
+              </Card>
+            </Grid>            
+            <Grid item xs={12} md={6}>
+              <Card sx={{ backgroundColor: '#1e1e1e', marginBottom: '10px', borderRadius: '10px' }}>
+                <CardContent>
+                  <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+                    <Typography variant="h6" gutterBottom sx={{ color: '#fff' }}>
+                        Showers Sum
+                    </Typography>
+                    <WavesIcon sx={{ fontSize: 48, color: '#fff', marginBottom: '10px' }} />
+                  </Box>
+                  <Typography variant="body1" sx={{ textAlign: 'center', color: '#fff', fontSize: '24px' }}>
+                    15 mm
+                  </Typography>
+                </CardContent>
+              </Card>
+            </Grid>            
+          </Grid>
       </Container>
     </Paper>
   );
